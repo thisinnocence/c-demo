19f58b199231e5acfe4e3b21aa2a084be751cb92
Michael <iamwuming@163.com>
2019-08-20 23:40:26 +0800

    add erase for c vector.

--- vector/vector.c
+++ vector/vector.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 struct vector {
     size_t item_size;
@@ -9,26 +10,27 @@ struct vector {
     void *head;
 };
 
-void ve_push_back(struct vector *ve, void *item) {
+void ve_push_back(struct vector *ve, void *item)
+{
     if (ve->item_num == ve->item_max_num) {
         unsigned int old_len = ve->item_size * ve->item_num;
-        ve->item_max_num *= 1.5;
+        ve->item_max_num += ve->item_max_num >> 1u;
         void *new_array = malloc(ve->item_size * ve->item_max_num);
+        if (new_array == NULL) {
+            printf("malloc failed");
+            return;
+        }
         memcpy(new_array, ve->head, old_len);
         free(ve->head);
         ve->head = new_array;
-    }
-    void *new_item = malloc(ve->item_size);
-    if (new_item == NULL) {
-        printf("malloc failed");
-        return;
     }
     void *pos = (char *) ve->head + ve->item_num * ve->item_size;
     memcpy(pos, item, ve->item_size);
     ve->item_num++;
 }
 
-void ve_erase(struct vector *ve, void *item) {
+void ve_erase(struct vector *ve, void *item)
+{
     if (item == NULL) {
         printf("erase nullptr\n");
         return;
@@ -50,27 +52,52 @@ void ve_erase(struct vector *ve, void *item) {
     }
 }
 
-void ve_init(struct vector *ve, size_t item_size) {
+typedef bool (ve_comp_func)(const char *item1, const char *item2);
+
+void *ve_find(struct vector *ve, void *item, ve_comp_func comp)
+{
+    size_t i = 0;
+    for (; i < ve->item_num; i++) {
+        char *cur = (char*)ve->head + i * ve->item_size;
+        if (comp((char*)item, cur)) {
+            return cur;
+        }
+    }
+    return NULL;
+}
+
+void ve_init(struct vector *ve, size_t item_size)
+{
     ve->item_max_num = 5;
     ve->head = malloc(ve->item_max_num * item_size);
     ve->item_size = item_size;
     ve->item_num = 0;
 }
 
-void ve_clear(struct vector *ve) {
+void ve_clear(struct vector *ve)
+{
     free(ve->head);
     memset(ve, 0, sizeof(*ve));
 }
 
-void show_re(struct vector *ve) {
-    int i;
+//--------- test case ------------------
+// an int vector
+void show_re(struct vector *ve)
+{
+    unsigned int i;
     int *data = (int *) ve->head;
     for (i = 0; i < ve->item_num; i++) {
         printf("%d ", data[i]);
     }
 }
 
-int main() {
+bool comp(const char *item1, const char *item2)
+{
+    return *(int*)item1 == *(int*)item2;
+}
+
+int main()
+{
     int n = 0;
     struct vector ve;
     ve_init(&ve, sizeof(int));
@@ -85,14 +112,7 @@ int main() {
 
     while (n != -2) {
         scanf("%d", &n);
-        int *item = NULL;
-        unsigned int i = 0;
-        for (i = 0; i < ve.item_num; i++) {
-            if (((int *) ve.head)[i] == n) {
-                item = &((int *) ve.head)[i];
-                break;
-            }
-        }
+        void *item = ve_find(&ve, &n, comp);
         ve_erase(&ve, item);
         show_re(&ve);
         printf("\n");

9498d439ebc451422b8a9f154d6013a31c59c900
Michael <iamwuming@163.com>
2019-09-17 08:03:48 +0800

    Add scanf usage.

---
+++ standord_io/scanf_test.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char tmp[100] = {0};
+    char tmp1[100] = {0};
+    int ret;
+
+    while ((ret = scanf("%s %s", tmp, tmp1)) != EOF) {
+        printf("ret:[%d]\t%s %s\n", ret, tmp, tmp1);
+        tmp[0] = '\0';
+        tmp1[0] = '\0';
+    }
+
+    printf("ret:[%d], EOF(%d)\n", ret, EOF);
+    return 0;
+}
+
+/*
+root@ubuntu:/media/sf_VMshare/test# echo "a hi abc abcd hello" | ./a.out
+ret:[2]	a hi
+ret:[2]	abc abcd
+ret:[1]	hello
+ret:[-1], EOF(-1)
+*/

db3bd0793d65a183a4aa25f87d549fa90c51b562
Michael <iamwuming@163.com>
2019-09-21 08:29:59 +0800

    Update ipv4 convert.

--- string_practice/sscanf_strtoul_test.c
+++ string_practice/sscanf_strtoul_test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main() {
     uint32_t ip;
@@ -13,25 +14,16 @@ int main() {
     char *val2 = (char*)&ip2;
 
     char *next = "192.168.1.1";
-    int n = 0, i = 3;
+    int i = 3;
     while (next && *next) {
-        // Reference to an object of type char*, whose value is set by the function to 
-        //   the next character in str after the numerical value.
-        // well, not ued often
-        val2[i] = strtoul(next, &next, 0);
-        printf("%hhu [%d %p %c]\n", val[i], i, next, *next);
-        i--;
-        if (*next == '.') next++;
+        val2[i--] = strtoul(next, &next, 0);
+        while(!isdigit(*next) && *next) next++;
     }
     printf("%u\n", ip2);
     return 0;
 }
 
-/*
+/* output:
 3232235777
-192 [3 0x106781f81 .]
-168 [2 0x106781f85 .]
-1 [1 0x106781f87 .]
-1 [0 0x106781f89 ]
 3232235777
 */

334bfbdec94140999db1dbaec50726083e001fc2
Michael <iamwuming@163.com>
2024-08-11 11:13:05 +0800

    refactor makefile.

--- systemc/async_event.cc
+++ systemc/async_event.cc
@@ -13,17 +13,21 @@ SC_MODULE(AsyncExample) {
     }
 
     void process_event() {
-        while (true) {
+        int cnt = 0;
+        for(;;) {
             wait(async_event);  // 等待异步事件触发
-            log("Async event caught at %s", sc_time_stamp().to_string().c_str());
+            log("Async event caught at %f cnt[%d]", sc_time_stamp().to_seconds(), cnt);
+            if (cnt++ > 1) {
+                exit(0);
+            }
         }
     }
 };
 
 void* os_thread(void* arg) {
     while (true) {
-        sleep(2);  // 模拟一些外部事件
-        log("OS thread: Triggering async event at %s", sc_time_stamp().to_string().c_str());
+        sleep(1);  // 模拟一些外部事件
+        log("OS thread: Triggering async event at %f", sc_time_stamp().to_seconds());
         async_event.notify();  // 异步通知 SystemC 事件
     }
     return nullptr;
@@ -44,24 +48,28 @@ int sc_main(int argc, char* argv[]) {
 OS线程可以和SC_THREAD交替运行，对于 event 只有SC调度的协程中触发使用，跨OS的thread需要使用这个实现。
 also see:  https://github.com/Xilinx/libsystemctlm-soc/blob/master/utils/async_event.h
 
-(gdb) info threads
-  Id   Target Id                                           Frame
-* 1    Thread 0x7ffff787b740 (LWP 16627) "async_event.out" AsyncExample::process_event (this=0x7fffffffda60) at async_event.cc:17
-  2    Thread 0x7ffff7476700 (LWP 16631) "async_event.out" 0x00007ffff7aac23f in __GI___clock_nanosleep (clock_id=clock_id@entry=0, 
-                                                            flags=flags@entry=0, req=req@entry=0x7ffff7475e50, rem=rem@entry=0x7ffff7475e50) 
-                                                            at ../sysdeps/unix/sysv/linux/clock_nanosleep.c:78
-(gdb) t a a bt
+[New Thread 0x7ffff7476700 (LWP 7374)]
+[11:10:00.401090][async_event.cc:30] OS thread: Triggering async event at 0.000000
+[11:10:00.401274][async_event.cc:19] Async event caught at 0.000000 cnt[0]
 
-Thread 2 (Thread 0x7ffff7476700 (LWP 16631)):
+Thread 1 "async_event.out" hit Breakpoint 1, AsyncExample::process_event (this=0x7fffffffda60) at async_event.cc:20
+warning: Source file is more recent than executable.
+
+(gdb) info threads
+  Id   Target Id                                          Frame
+* 1    Thread 0x7ffff787b740 (LWP 7370) "async_event.out" AsyncExample::process_event (this=0x7fffffffda60) at async_event.cc:20
+  2    Thread 0x7ffff7476700 (LWP 7374) "async_event.out" 0x00007ffff7aac23f in __GI___clock_nanosleep () at ../sysdeps/unix/sysv/linux/clock_nanosleep.c:78
+
+(gdb) t a a bt
 #0  0x00007ffff7aac23f in __GI___clock_nanosleep () at ../sysdeps/unix/sysv/linux/clock_nanosleep.c:78
-#1  0x00007ffff7ab1ec7 in __GI___nanosleep () at nanosleep.c:27
+#1  0x00007ffff7ab1ec7 in __GI___nanosleep (requested_time=requested_time@entry=0x7ffff7475e90, remaining=remaining@entry=0x7ffff7475e90) at nanosleep.c:27
 #2  0x00007ffff7ab1dfe in __sleep (seconds=0) at ../sysdeps/posix/sleep.c:55
-#3  0x0000555555557611 in os_thread (arg=0x0) at async_event.cc:25
+#3  0x00005555555575d0 in os_thread (arg=0x0) at async_event.cc:29
 #4  0x00007ffff7dc6609 in start_thread (arg=<optimized out>) at pthread_create.c:477
 #5  0x00007ffff7aee133 in clone () at ../sysdeps/unix/sysv/linux/x86_64/clone.S:95
 
-Thread 1 (Thread 0x7ffff787b740 (LWP 16627)):
-#0  AsyncExample::process_event (this=0x7fffffffda60) at async_event.cc:17
+Thread 1 (Thread 0x7ffff787b740 (LWP 7370)):
+#0  AsyncExample::process_event (this=0x7fffffffda60) at async_event.cc:20
 #1  0x00007ffff7eac79f in sc_core::sc_process_b::semantics (this=0x5555555715b0) at ../../../src/sysc/kernel/sc_process.h:633
 #2  sc_core::sc_thread_cor_fn (arg=0x5555555715b0) at ../../../src/sysc/kernel/sc_thread_process.cpp:117
 #3  0x00007ffff7f68c2b in qt_blocki () at qtmds.s:71
